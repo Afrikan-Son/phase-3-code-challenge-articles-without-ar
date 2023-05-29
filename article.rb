427256d4427d97d7db5fbb1e06e5085bf759740e
Mark Wanjau <theafrikanson@gmail.com>
2023-05-29 17:24:37 +0300

    method comments for articles

---
+++ article.rb
@@ -0,0 +1,6 @@
+#initialize(name): Initialise an author with a name
+#name: Return the name of the autho\r
+#articles: Return an arrayof Article\ instances the author has written
+#magazines: Return a uique array of Magazine instances for which the author has contributed to
+#add_article(magazine, title): Create a new Artile instance asociated with the author and the specified magazine
+#topic_areas: Return a uique array of strings with the catgries of the magazines the author has contributed to
