56056fc5b447876009c4f0a26a34c47ee09c8d0a
Mark Wanjau <theafrikanson@gmail.com>
2023-05-29 17:28:34 +0300

    author methods

--- article.rb
+++ article.rb
@@ -1,6 +1,4 @@
-#initialize(name): Initialise an author with a name
-#name: Return the name of the autho\r
-#articles: Return an arrayof Article\ instances the author has written
-#magazines: Return a uique array of Magazine instances for which the author has contributed to
-#add_article(magazine, title): Create a new Artile instance asociated with the author and the specified magazine
-#topic_areas: Return a uique array of strings with the catgries of the magazines the author has contributed to
+#initialize(author, magazine, title):Initialize an article with an author (Author object), a magazine (Magazine object)and a title (string)
+#title: Return the title of the article
+#author: Return the author of the articl
+#magazine: Return the magazine of the article
