f8c8292012f2ef6937e93011ce0c5518385beb11
Mark Wanjau <theafrikanson@gmail.com>
2023-05-29 20:50:40 +0300

    update author.rb

--- author.rb
+++ author.rb
@@ -34,3 +34,20 @@ class Author
 end
 
 =end
+
+class Author
+    attr_reader :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+   
+end
