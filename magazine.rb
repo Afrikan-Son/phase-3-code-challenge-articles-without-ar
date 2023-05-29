5e231a868a9d572e243188e4ffd9d1f3833e68ed
Mark Wanjau <theafrikanson@gmail.com>
2023-05-29 17:40:53 +0300

    magazine code

--- magazine.rb
+++ magazine.rb
@@ -8,3 +8,37 @@ self.all: Return an aray of all Magzine instances
 self.find_by_name(name): Given a string of a magzine's name, return the first magazine object that matchs
 contributing_authors: Return an array of auhors who have written more than 2 articles for the magazine
 =end
+
+
+class Magazine
+    attr_accessor :name, :category
+  
+    @@all = []
+  
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def contributors
+      Article.all.select { |article| article.magazine == self }.map(&:author)
+    end
+  
+    def article_titles
+      Article.all.select { |article| article.magazine == self }.map(&:title)
+    end
+  
+    def self.find_by_name(name)
+      @@all.find { |magazine| magazine.name == name }
+    end
+  
+    def contributing_authors
+      contributors.group_by(&:name).select { |_, authors| authors.length > 2 }.values.flatten
+    end
+end
+  
