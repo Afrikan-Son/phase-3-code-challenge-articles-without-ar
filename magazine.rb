5c4f391ddd11214fd5f6072ab5f910c0f9dad03b
Mark Wanjau <theafrikanson@gmail.com>
2023-05-29 20:52:32 +0300

    magazinerb

--- magazine.rb
+++ magazine.rb
@@ -43,4 +43,19 @@ end
 =end
 
 
+class Magazine
+    attr_accessor :name, :category
   
+    @@all = []
+  
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+end
